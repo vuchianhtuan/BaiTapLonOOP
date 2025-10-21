60c28dbba3981be91ef00879939371eb050eb868
truong2595 <truong2592005@gmail.com>
2025-10-22 02:06:35 +0700

    feat(objects): Thêm các object track, thumb phục vụ Setup

---
+++ src/main/java/com/mygame/arkanoid/objects/Track.java
@@ -0,0 +1,37 @@
+package com.mygame.arkanoid.objects;
+
+import com.mygame.arkanoid.engine.AssetManager;
+import com.mygame.arkanoid.systems.ScalingManager;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Track extends GameObject {
+    String imageName;
+
+    public Track(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        this.imageName = "track";
+    }
+
+    @Override
+    public void update() {
+        // Cập nhật logic của Track nếu cần thiết
+
+    }
+
+    @Override
+    public void render(Graphics g, ScalingManager sm) {
+
+        BufferedImage img = AssetManager.getInstance().getImage(this.imageName);
+        if (img != null) {
+            g.drawImage(img,
+                    sm.scaleX(this.x), sm.scaleY(this.y),
+                    sm.scaleWidth(this.width), sm.scaleHeight(this.height), null);
+        } else {
+            g.setColor(Color.GRAY);
+            g.fillRect(sm.scaleX(this.x), sm.scaleY(this.y),
+                    sm.scaleWidth(this.width), sm.scaleHeight(this.height));
+        }
+    }
+}
