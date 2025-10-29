66034569f16f7b000a220faefbbb886e4e05e338
tuanhoangnghia <hoangnghiatuan297@gmail.com>
2025-10-29 20:23:20 +0700

    feat(UI): Bổ sung thêm assets mới cho powerup.

--- src/main/java/com/mygame/arkanoid/core/GameManager.java
+++ src/main/java/com/mygame/arkanoid/core/GameManager.java
@@ -844,20 +844,6 @@ public class GameManager {
         newPowerUp.applyEffect(this);
     }
 
-    /*
-    private PowerUp createPowerUp(PowerUpType type, int x, int y) {
-        switch (type) {
-            case EXPAND: return new ExpandPaddlePowerUp(x, y, 63, 30);
-            case STICKY: return new StickyPaddlePowerUp(x, y, 59, 30);
-            case SLOW_BALL: return new SlowBallPowerUp(x, y, 63, 30);
-            case FAST_BALL: return new FastBallPowerUp(x, y, 63, 30);
-            case EXTRA_LIFE: return new ExtraLifePowerUp(x, y, 30, 30);
-            case MULTI_BALL: return new MultiBallPowerUp(x, y, 61, 30);
-            default: return null;
-        }
-    }
-     */
-
     public void ensureBrickIdsAssigned() {
         int idCounter = 0;
         if (levelManager != null && levelManager.getCurrentLevel() != null) {
--- src/main/java/com/mygame/arkanoid/objects/powerups/PowerUpFactory.java
+++ src/main/java/com/mygame/arkanoid/objects/powerups/PowerUpFactory.java
@@ -7,12 +7,12 @@ public final class PowerUpFactory {
 
     public static PowerUp create(PowerUpType type, int x, int y) {
         switch (type) {
-            case EXPAND:      return new ExpandPaddlePowerUp(x, y, SIZE, SIZE);
-            case STICKY:      return new StickyPaddlePowerUp(x, y, SIZE, SIZE);
-            case SLOW_BALL:   return new SlowBallPowerUp(x, y, SIZE, SIZE);
-            case FAST_BALL:   return new FastBallPowerUp(x, y, SIZE, SIZE);
-            case EXTRA_LIFE:  return new ExtraLifePowerUp(x, y, SIZE, SIZE);
-            case MULTI_BALL:  return new MultiBallPowerUp(x, y, SIZE, SIZE);
+            case EXPAND: return new ExpandPaddlePowerUp(x, y, 63, 30);
+            case STICKY: return new StickyPaddlePowerUp(x, y, 59, 30);
+            case SLOW_BALL: return new SlowBallPowerUp(x, y, 63, 30);
+            case FAST_BALL: return new FastBallPowerUp(x, y, 63, 30);
+            case EXTRA_LIFE: return new ExtraLifePowerUp(x, y, 30, 30);
+            case MULTI_BALL: return new MultiBallPowerUp(x, y, 61, 30);
             default:
                 throw new IllegalArgumentException("Unsupported PowerUpType: " + type);
         }
